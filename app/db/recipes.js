e85e69bc741304ae69334e0294f20fed715edcd3
Harry Ganz <harry.y.ganz@gmail.com>
2016-02-28 11:43:37 -0500

    Can list recipes by category

--- app/db/recipes.js
+++ app/db/recipes.js
@@ -4,19 +4,29 @@ var pgp = require('pg-promise')();
 var db = pgp(process.env.DATABASE_URL);
 
 
-function listAll(req, res, next){
 
-  db.any(
-    `SELECT recipes.id AS id, recipes.title AS title, users.username AS user,
-    ARRAY_AGG(categories.category) as categories FROM
-    recipes LEFT JOIN users ON
-    recipes.user_id = users.id
-    LEFT JOIN categories_recipes_xref AS crx ON
-    recipes.id = crx.recipe_id
-    LEFT JOIN categories ON
-    categories.id = crx.category_id
-    WHERE recipes.is_shown
-    GROUP BY recipes.id, recipes.title, users.username;`).
+function listAll(req, res, next){
+  var categories = req.query.category;
+  var query = `SELECT recipes.id AS id, recipes.title AS title,
+  users.username AS user, ARRAY_AGG(categories.category) AS categories
+      FROM (SELECT recipes.id AS recipeid FROM recipes
+          INNER JOIN categories_recipes_xref AS crx ON
+          crx.recipe_id = recipes.id
+          INNER JOIN categories ON
+          crx.category_id = categories.id
+          ${categories ? 'AND categories.category IN ($1^)' : ''}
+          GROUP BY recipeid) AS rcat
+  INNER JOIN recipes ON
+  recipes.id = rcat.recipeid
+  INNER JOIN categories_recipes_xref AS crx ON
+  crx.recipe_id = recipes.id
+  INNER JOIN categories ON
+  categories.id = crx.category_id
+  INNER JOIN users ON
+  recipes.user_id = users.id
+  WHERE recipes.is_shown
+  GROUP BY recipes.id, recipes.title, users.username;`;
+  db.any(query, pgp.as.csv(categories)).
     then(function(results){
       res.data = results;
       next();
