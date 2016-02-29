8995688dafecdc8fbefc1fac5d50ebf66ba6bbf5
Harry Ganz <harry.y.ganz@gmail.com>
2016-02-29 16:57:19 -0500

    Edited database seed to get rid of user added content

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,7 @@
 BEGIN;
   INSERT INTO categories (category) VALUES
-  ('Vegan'), ('Vegetarian'), ('Kosher'), ('Peanut Allergy');
-
-  INSERT INTO users (username, email) VALUES
-  ('seifdin', 'target@example.com'),
-  ('user2', 'user2@example.com');
-
-  INSERT INTO recipes (title, directions, user_id) VALUES
-  ('Title 1', 'This is the first descriptions fjjfknlnsfknsdf,nsndf.jns.djfn.jn',
-  1),
-  ('Title 2', 'Another desciptions hrhjbrkhbfhbsljcbhsbchsbcljhbsch shc sjhc ,sh cs ',
-  2);
-
-  INSERT INTO ingredients (ingredient) VALUES
-  ('Red Pepper'), ('Beef'), ('Green Pepper'), ('Sliced Onion'),
-  ('Ketchup'), ('Salsa Verde');
-
-  INSERT INTO categories_recipes_xref (recipe_id, category_id) VALUES
-  (1,1), (1,2), (2,1), (2,2), (2,3), (2,4);
-
-  INSERT INTO ingredients_recipes_xref (recipe_id, ingredient_id) VALUES
-  (1,1), (1,3), (1,4),(2,2), (2,5), (2,6);
+  ('Vegan'), ('Vegetarian'), ('Kosher'),
+  ('Freegan'), ('Halal'), ('Gluten Free'),
+  ('Soy Allergy'), ('Non-Dairy'), ('Peanut Allergy');
 
 COMMIT;
